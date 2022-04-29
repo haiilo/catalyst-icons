1e2645ad1558cb9d28a0c220303bd9848a2ac3b2
markus-coyo <98806154+markus-coyo@users.noreply.github.com>
2022-04-29 15:46:57 +0000

    Deploying to gh-pages from @ haiilo/catalyst-icons@cb5a86290ceb6c1429eec6a6f8d47f43a17edaf4 🚀

--- js/icons.constants.d.ts
+++ js/icons.constants.d.ts
@@ -654,6 +654,170 @@ export declare const ciTwitter: {
     name: 'twitter';
     data: string;
 };
+export declare const ciWAlertOutlined: {
+    name: 'w-alert-outlined';
+    data: string;
+};
+export declare const ciWBirthdaysUpcomingOutlined: {
+    name: 'w-birthdays-upcoming-outlined';
+    data: string;
+};
+export declare const ciWBlogArticleNewOutlined: {
+    name: 'w-blog-article-new-outlined';
+    data: string;
+};
+export declare const ciWBlogArticleOutlined: {
+    name: 'w-blog-article-outlined';
+    data: string;
+};
+export declare const ciWBookmarksOutlined: {
+    name: 'w-bookmarks-outlined';
+    data: string;
+};
+export declare const ciWButtonOutlined: {
+    name: 'w-button-outlined';
+    data: string;
+};
+export declare const ciWColleaguesNewOutlined: {
+    name: 'w-colleagues-new-outlined';
+    data: string;
+};
+export declare const ciWCompleteProfileOutlined: {
+    name: 'w-complete-profile-outlined';
+    data: string;
+};
+export declare const ciWDividerOutlined: {
+    name: 'w-divider-outlined';
+    data: string;
+};
+export declare const ciWDoYouKnowOutlined: {
+    name: 'w-do-you-know-outlined';
+    data: string;
+};
+export declare const ciWDocumentOutlined: {
+    name: 'w-document-outlined';
+    data: string;
+};
+export declare const ciWDocumentsM365Outlined: {
+    name: 'w-documents-m-365-outlined';
+    data: string;
+};
+export declare const ciWDocumentsNewOutlined: {
+    name: 'w-documents-new-outlined';
+    data: string;
+};
+export declare const ciWDownloadOutlined: {
+    name: 'w-download-outlined';
+    data: string;
+};
+export declare const ciWEventsUpcomingOutlined: {
+    name: 'w-events-upcoming-outlined';
+    data: string;
+};
+export declare const ciWFacegameOutlined: {
+    name: 'w-facegame-outlined';
+    data: string;
+};
+export declare const ciWFeedOutlined: {
+    name: 'w-feed-outlined';
+    data: string;
+};
+export declare const ciWGalleryOutlined: {
+    name: 'w-gallery-outlined';
+    data: string;
+};
+export declare const ciWHashtagsOutlined: {
+    name: 'w-hashtags-outlined';
+    data: string;
+};
+export declare const ciWHeadlineOutlined: {
+    name: 'w-headline-outlined';
+    data: string;
+};
+export declare const ciWHeroTeaserOutlined: {
+    name: 'w-hero-teaser-outlined';
+    data: string;
+};
+export declare const ciWHtmlOutlined: {
+    name: 'w-html-outlined';
+    data: string;
+};
+export declare const ciWIframeOutlined: {
+    name: 'w-iframe-outlined';
+    data: string;
+};
+export declare const ciWImageOutlined: {
+    name: 'w-image-outlined';
+    data: string;
+};
+export declare const ciWLaunchpadOutlined: {
+    name: 'w-launchpad-outlined';
+    data: string;
+};
+export declare const ciWMediaOutlined: {
+    name: 'w-media-outlined';
+    data: string;
+};
+export declare const ciWOnlineUsersOutlined: {
+    name: 'w-online-users-outlined';
+    data: string;
+};
+export declare const ciWPluginTrelloOutlined: {
+    name: 'w-plugin-trello-outlined';
+    data: string;
+};
+export declare const ciWPluginTwitterOutlined: {
+    name: 'w-plugin-twitter-outlined';
+    data: string;
+};
+export declare const ciWPollOutlined: {
+    name: 'w-poll-outlined';
+    data: string;
+};
+export declare const ciWProfileOutlined: {
+    name: 'w-profile-outlined';
+    data: string;
+};
+export declare const ciWRssFeedOutlined: {
+    name: 'w-rss-feed-outlined';
+    data: string;
+};
+export declare const ciWSubscriptionsOutlined: {
+    name: 'w-subscriptions-outlined';
+    data: string;
+};
+export declare const ciWSuggestedCommunitiesOutlined: {
+    name: 'w-suggested-communities-outlined';
+    data: string;
+};
+export declare const ciWSuggestedPagesOutlined: {
+    name: 'w-suggested-pages-outlined';
+    data: string;
+};
+export declare const ciWTeaserOutlined: {
+    name: 'w-teaser-outlined';
+    data: string;
+};
+export declare const ciWTextOutlined: {
+    name: 'w-text-outlined';
+    data: string;
+};
+export declare const ciWVideoOutlined: {
+    name: 'w-video-outlined';
+    data: string;
+};
+export declare const ciWWelcomeOutlined: {
+    name: 'w-welcome-outlined';
+    data: string;
+};
+export declare const ciWWikiArticleNewOutlined: {
+    name: 'w-wiki-article-new-outlined';
+    data: string;
+};
+export declare const ciWWikiArticleOutlined: {
+    name: 'w-wiki-article-outlined';
+    data: string;
+};
 export declare const ciWechat: {
     name: 'wechat';
     data: string;
@@ -662,7 +826,7 @@ export declare const ciXing: {
     name: 'xing';
     data: string;
 };
-export declare type coyoIcon = '16-chevron-down' | '16-chevron-left' | '16-chevron-right' | '16-chevron-up' | 'arrow-down-outlined' | 'arrow-left-outlined' | 'arrow-right-outlined' | 'arrow-up-outlined' | 'article-filled' | 'article-outlined' | 'bar-chart-outlined' | 'bell-check-outlined' | 'bell-dot-filled' | 'bell-dot-outlined' | 'bell-filled' | 'bell-outlined' | 'bell-plus-outlined' | 'bell-ringing-filled' | 'bell-ringing-outlined' | 'book-filled' | 'book-outlined' | 'bookmark-check-outlined' | 'bookmark-filled' | 'bookmark-outlined' | 'bookmark-plus-outlined' | 'chat-filled' | 'chat-outlined' | 'check-circle-filled' | 'check-circle-outlined' | 'check-outlined' | 'chevron-down-outlined' | 'chevron-left-outlined' | 'chevron-right-outlined' | 'chevron-up-outlined' | 'clock-filled' | 'clock-outlined' | 'communities-filled' | 'communities-outlined' | 'content-filled' | 'content-outlined' | 'copy-outlined' | 'cross-circle-filled' | 'cross-circle-outlined' | 'cross-outlined' | 'cross-square-filled' | 'cross-square-outlined' | 'dots-circle-outlined' | 'download-outlined' | 'earth-outlined' | 'events-filled' | 'events-outlined' | 'folder-minus-outlined' | 'folder-outlined' | 'folder-plus-outlined' | 'gift-outlined' | 'globe-outlined' | 'gradient-outlined' | 'group-outlined' | 'hashtag-outlined' | 'home-filled' | 'home-outlined' | 'layers-filled' | 'layers-outlined' | 'likes-circle-filled' | 'link-outlined' | 'lock-filled' | 'lock-open-filled' | 'lock-open-outlined' | 'lock-outlined' | 'marketplace-filled' | 'marketplace-outlined' | 'maybe-circle-outlined' | 'megaphone-filled' | 'megaphone-outlined' | 'menu-outlined' | 'microsoft-teams' | 'microsoft-yammer' | 'paperclip-box-outlined' | 'paperclip-outlined' | 'photo-add-outlined' | 'photo-filled' | 'photo-outlined' | 'photo-remove-outlined' | 'plus-circle-filled' | 'plus-circle-outlined' | 'plus-outlined' | 'plus-square-filled' | 'plus-square-outlined' | 'printer-filled' | 'printer-outlined' | 'profile-box-outlined' | 'profile-circle-outlined' | 'profiles-filled' | 'profiles-outlined' | 'question-mark-circle-outlined' | 'reading-glasses-filled' | 'reading-glasses-outlined' | 'search-filled' | 'search-outlined' | 'settings-outlined' | 'share-filled' | 'share-outlined' | 'sign-in-outlined' | 'sign-out-outlined' | 'signal-outlined' | 'slack' | 'smile' | 'sp-grid-2-cols' | 'sp-grid-3-cols' | 'sp-grid-4-cols' | 'sp-grid-full' | 'sp-grid-sidebar-left-lg' | 'sp-grid-sidebar-left' | 'sp-grid-sidebar-right-lg' | 'sp-grid-sidebar-right' | 'sp-grid-social' | 'sparkle-filled' | 'sparkle-outlined' | 'star-circle-filled' | 'star-circle-outlined' | 'sticker-filled' | 'sticker-outlined' | 'thumbs-up-filled' | 'thumbs-up-outlined' | 'translate-outlined' | 'trash-filled' | 'trash-outlined' | 'upload-outlined' | 'video-camera-filled' | 'video-camera-outlined' | 'w-blog-article-recommended-outlined' | 'write-outlined' | 'apple-safari' | 'facebook' | 'feed-filled' | 'feed-outlined' | 'fingerprint-outlined' | 'google-calendar' | 'google-chrome' | 'google-drive' | 'google-mail' | 'google' | 'haiilo-home' | 'haiilo-insights' | 'haiilo-outlined' | 'haiilo-share' | 'haiilo-stories' | 'instagram' | 'linkedin' | 'mail-open-outlined' | 'mail-outlined' | 'microsoft-edge' | 'microsoft-excel' | 'microsoft-onenote' | 'microsoft-outlook' | 'microsoft-planner' | 'microsoft-powerpoint' | 'microsoft-sharepoint' | 'microsoft-word' | 'microsoft' | 'mozilla-firefox' | 'stream-filled' | 'stream-outlined' | 'twitter' | 'wechat' | 'xing';
+export declare type coyoIcon = '16-chevron-down' | '16-chevron-left' | '16-chevron-right' | '16-chevron-up' | 'arrow-down-outlined' | 'arrow-left-outlined' | 'arrow-right-outlined' | 'arrow-up-outlined' | 'article-filled' | 'article-outlined' | 'bar-chart-outlined' | 'bell-check-outlined' | 'bell-dot-filled' | 'bell-dot-outlined' | 'bell-filled' | 'bell-outlined' | 'bell-plus-outlined' | 'bell-ringing-filled' | 'bell-ringing-outlined' | 'book-filled' | 'book-outlined' | 'bookmark-check-outlined' | 'bookmark-filled' | 'bookmark-outlined' | 'bookmark-plus-outlined' | 'chat-filled' | 'chat-outlined' | 'check-circle-filled' | 'check-circle-outlined' | 'check-outlined' | 'chevron-down-outlined' | 'chevron-left-outlined' | 'chevron-right-outlined' | 'chevron-up-outlined' | 'clock-filled' | 'clock-outlined' | 'communities-filled' | 'communities-outlined' | 'content-filled' | 'content-outlined' | 'copy-outlined' | 'cross-circle-filled' | 'cross-circle-outlined' | 'cross-outlined' | 'cross-square-filled' | 'cross-square-outlined' | 'dots-circle-outlined' | 'download-outlined' | 'earth-outlined' | 'events-filled' | 'events-outlined' | 'folder-minus-outlined' | 'folder-outlined' | 'folder-plus-outlined' | 'gift-outlined' | 'globe-outlined' | 'gradient-outlined' | 'group-outlined' | 'hashtag-outlined' | 'home-filled' | 'home-outlined' | 'layers-filled' | 'layers-outlined' | 'likes-circle-filled' | 'link-outlined' | 'lock-filled' | 'lock-open-filled' | 'lock-open-outlined' | 'lock-outlined' | 'marketplace-filled' | 'marketplace-outlined' | 'maybe-circle-outlined' | 'megaphone-filled' | 'megaphone-outlined' | 'menu-outlined' | 'microsoft-teams' | 'microsoft-yammer' | 'paperclip-box-outlined' | 'paperclip-outlined' | 'photo-add-outlined' | 'photo-filled' | 'photo-outlined' | 'photo-remove-outlined' | 'plus-circle-filled' | 'plus-circle-outlined' | 'plus-outlined' | 'plus-square-filled' | 'plus-square-outlined' | 'printer-filled' | 'printer-outlined' | 'profile-box-outlined' | 'profile-circle-outlined' | 'profiles-filled' | 'profiles-outlined' | 'question-mark-circle-outlined' | 'reading-glasses-filled' | 'reading-glasses-outlined' | 'search-filled' | 'search-outlined' | 'settings-outlined' | 'share-filled' | 'share-outlined' | 'sign-in-outlined' | 'sign-out-outlined' | 'signal-outlined' | 'slack' | 'smile' | 'sp-grid-2-cols' | 'sp-grid-3-cols' | 'sp-grid-4-cols' | 'sp-grid-full' | 'sp-grid-sidebar-left-lg' | 'sp-grid-sidebar-left' | 'sp-grid-sidebar-right-lg' | 'sp-grid-sidebar-right' | 'sp-grid-social' | 'sparkle-filled' | 'sparkle-outlined' | 'star-circle-filled' | 'star-circle-outlined' | 'sticker-filled' | 'sticker-outlined' | 'thumbs-up-filled' | 'thumbs-up-outlined' | 'translate-outlined' | 'trash-filled' | 'trash-outlined' | 'upload-outlined' | 'video-camera-filled' | 'video-camera-outlined' | 'w-blog-article-recommended-outlined' | 'write-outlined' | 'apple-safari' | 'facebook' | 'feed-filled' | 'feed-outlined' | 'fingerprint-outlined' | 'google-calendar' | 'google-chrome' | 'google-drive' | 'google-mail' | 'google' | 'haiilo-home' | 'haiilo-insights' | 'haiilo-outlined' | 'haiilo-share' | 'haiilo-stories' | 'instagram' | 'linkedin' | 'mail-open-outlined' | 'mail-outlined' | 'microsoft-edge' | 'microsoft-excel' | 'microsoft-onenote' | 'microsoft-outlook' | 'microsoft-planner' | 'microsoft-powerpoint' | 'microsoft-sharepoint' | 'microsoft-word' | 'microsoft' | 'mozilla-firefox' | 'stream-filled' | 'stream-outlined' | 'twitter' | 'w-alert-outlined' | 'w-birthdays-upcoming-outlined' | 'w-blog-article-new-outlined' | 'w-blog-article-outlined' | 'w-bookmarks-outlined' | 'w-button-outlined' | 'w-colleagues-new-outlined' | 'w-complete-profile-outlined' | 'w-divider-outlined' | 'w-do-you-know-outlined' | 'w-document-outlined' | 'w-documents-m-365-outlined' | 'w-documents-new-outlined' | 'w-download-outlined' | 'w-events-upcoming-outlined' | 'w-facegame-outlined' | 'w-feed-outlined' | 'w-gallery-outlined' | 'w-hashtags-outlined' | 'w-headline-outlined' | 'w-hero-teaser-outlined' | 'w-html-outlined' | 'w-iframe-outlined' | 'w-image-outlined' | 'w-launchpad-outlined' | 'w-media-outlined' | 'w-online-users-outlined' | 'w-plugin-trello-outlined' | 'w-plugin-twitter-outlined' | 'w-poll-outlined' | 'w-profile-outlined' | 'w-rss-feed-outlined' | 'w-subscriptions-outlined' | 'w-suggested-communities-outlined' | 'w-suggested-pages-outlined' | 'w-teaser-outlined' | 'w-text-outlined' | 'w-video-outlined' | 'w-welcome-outlined' | 'w-wiki-article-new-outlined' | 'w-wiki-article-outlined' | 'wechat' | 'xing';
 export interface CoyoIcon {
     name: coyoIcon;
     data: string;
@@ -1159,6 +1323,129 @@ export declare const completeIconSet: ({
     data: string;
 } | {
     name: 'twitter';
+    data: string;
+} | {
+    name: 'w-alert-outlined';
+    data: string;
+} | {
+    name: 'w-birthdays-upcoming-outlined';
+    data: string;
+} | {
+    name: 'w-blog-article-new-outlined';
+    data: string;
+} | {
+    name: 'w-blog-article-outlined';
+    data: string;
+} | {
+    name: 'w-bookmarks-outlined';
+    data: string;
+} | {
+    name: 'w-button-outlined';
+    data: string;
+} | {
+    name: 'w-colleagues-new-outlined';
+    data: string;
+} | {
+    name: 'w-complete-profile-outlined';
+    data: string;
+} | {
+    name: 'w-divider-outlined';
+    data: string;
+} | {
+    name: 'w-do-you-know-outlined';
+    data: string;
+} | {
+    name: 'w-document-outlined';
+    data: string;
+} | {
+    name: 'w-documents-m-365-outlined';
+    data: string;
+} | {
+    name: 'w-documents-new-outlined';
+    data: string;
+} | {
+    name: 'w-download-outlined';
+    data: string;
+} | {
+    name: 'w-events-upcoming-outlined';
+    data: string;
+} | {
+    name: 'w-facegame-outlined';
+    data: string;
+} | {
+    name: 'w-feed-outlined';
+    data: string;
+} | {
+    name: 'w-gallery-outlined';
+    data: string;
+} | {
+    name: 'w-hashtags-outlined';
+    data: string;
+} | {
+    name: 'w-headline-outlined';
+    data: string;
+} | {
+    name: 'w-hero-teaser-outlined';
+    data: string;
+} | {
+    name: 'w-html-outlined';
+    data: string;
+} | {
+    name: 'w-iframe-outlined';
+    data: string;
+} | {
+    name: 'w-image-outlined';
+    data: string;
+} | {
+    name: 'w-launchpad-outlined';
+    data: string;
+} | {
+    name: 'w-media-outlined';
+    data: string;
+} | {
+    name: 'w-online-users-outlined';
+    data: string;
+} | {
+    name: 'w-plugin-trello-outlined';
+    data: string;
+} | {
+    name: 'w-plugin-twitter-outlined';
+    data: string;
+} | {
+    name: 'w-poll-outlined';
+    data: string;
+} | {
+    name: 'w-profile-outlined';
+    data: string;
+} | {
+    name: 'w-rss-feed-outlined';
+    data: string;
+} | {
+    name: 'w-subscriptions-outlined';
+    data: string;
+} | {
+    name: 'w-suggested-communities-outlined';
+    data: string;
+} | {
+    name: 'w-suggested-pages-outlined';
+    data: string;
+} | {
+    name: 'w-teaser-outlined';
+    data: string;
+} | {
+    name: 'w-text-outlined';
+    data: string;
+} | {
+    name: 'w-video-outlined';
+    data: string;
+} | {
+    name: 'w-welcome-outlined';
+    data: string;
+} | {
+    name: 'w-wiki-article-new-outlined';
+    data: string;
+} | {
+    name: 'w-wiki-article-outlined';
     data: string;
 } | {
     name: 'wechat';
