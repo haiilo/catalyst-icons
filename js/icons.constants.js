6709821b7a34bea87d76303a43e6efd91e676504
markus-coyo <98806154+markus-coyo@users.noreply.github.com>
2022-07-10 10:00:59 +0000

    Deploying to gh-pages from @ haiilo/catalyst-icons@77cf1f84989ac1f53171943e8a12a0c1937de867 🚀

--- js/icons.constants.js
+++ js/icons.constants.js
@@ -743,13 +743,13 @@ export const ciHistoryOutlined = {
     name: 'history-outlined',
     data: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M10 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm2.255-.027a.75.75 0 0 1 .827-.664c4.876.54 8.668 4.672 8.668 9.691A9.75 9.75 0 0 1 12 21.75c-5.02 0-9.152-3.792-9.69-8.668a.75.75 0 0 1 1.49-.164A8.251 8.251 0 1 0 12.918 3.8a.75.75 0 0 1-.663-.827Z"/><path d="M12 6.25a.75.75 0 0 1 .75.75v4.25H17a.75.75 0 0 1 0 1.5h-5a.75.75 0 0 1-.75-.75V7a.75.75 0 0 1 .75-.75ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm2-4a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"/></svg>`
 };
+export const ciInfoCircleFilled = {
+    name: 'info-circle-filled',
+    data: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM13 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm0 3.5a1 1 0 1 0-2 0V16a1 1 0 1 0 2 0v-4.5Z"/></svg>`
+};
 export const ciInfoCircleOutlined = {
     name: 'info-circle-outlined',
     data: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm-.25 3.5a.75.75 0 0 0-1.5 0V16a.75.75 0 0 0 1.5 0v-4.5Z"/><path fill-rule="evenodd" clip-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 3.75a8.25 8.25 0 1 0 0 16.5 8.25 8.25 0 0 0 0-16.5Z"/></svg>`
-};
-export const ciInfoCircledFilled = {
-    name: 'info-circled-filled',
-    data: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM13 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm0 3.5a1 1 0 1 0-2 0V16a1 1 0 1 0 2 0v-4.5Z"/></svg>`
 };
 export const ciInstagram = {
     name: 'instagram',
@@ -1222,8 +1222,8 @@ export const completeIconSet = [
     ciHaiiloShare,
     ciHaiiloStories,
     ciHistoryOutlined,
+    ciInfoCircleFilled,
     ciInfoCircleOutlined,
-    ciInfoCircledFilled,
     ciInstagram,
     ciLinkBreakOutlined,
     ciLinkedin,
