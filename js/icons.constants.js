a6cb4fda93e62873e1aa988b82dbe840bd4bdc31
denise-hertwig <122547626+denise-hertwig@users.noreply.github.com>
2023-01-17 12:37:11 +0000

    Deploying to gh-pages from @ haiilo/catalyst-icons@b6ac99461b38d8f84c3b5151d36cfe304062774d 🚀

--- js/icons.constants.js
+++ js/icons.constants.js
@@ -801,7 +801,7 @@ export const ciCircleFilled = {
 };
 export const ciCircleOutlinedS = {
     name: 'circle-outlined-s',
-    data: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 7.75a4.25 4.25 0 1 0 0 8.5 4.25 4.25 0 0 0 0-8.5ZM6.25 12a5.75 5.75 0 1 1 11.5 0 5.75 5.75 0 0 1-11.5 0Z"/></svg>`
+    data: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 8.75a3.25 3.25 0 1 0 0 6.5 3.25 3.25 0 0 0 0-6.5ZM7.25 12a4.75 4.75 0 1 1 9.5 0 4.75 4.75 0 0 1-9.5 0Z"/></svg>`
 };
 export const ciCircleOutlined = {
     name: 'circle-outlined',
